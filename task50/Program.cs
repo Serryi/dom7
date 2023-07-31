a146f12bc81fb4259913e900d783a8b5c35dc380
Sergei Zubov <tehwas@ya.ru>
2023-07-31 20:37:35 +0300

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int row = ReadInt("Введите индекс строки: ");
+int column = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[5, 6];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if (row < numbers.GetLength(0) && column < numbers.GetLength(1)) Console.WriteLine(numbers[row, column]);
+else Console.WriteLine($"{row}{column} -> такого числа в массиве нет");
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
